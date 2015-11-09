663e55e89431b7e29aacafcab3a642a667c1c4ab
Géraud <gbeguin@xebia.fr>
2015-11-10 00:13:23 +0100

    Added share intents

--- app/src/main/java/org/gbe/popularmovies/ui/MovieListActivity.java
+++ app/src/main/java/org/gbe/popularmovies/ui/MovieListActivity.java
@@ -86,11 +86,11 @@ public class MovieListActivity extends AppCompatActivity
     public void onVideoClicked(Video video) {
         try {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + video.getKey()));
-            startActivity(intent);
+            startActivity(Intent.createChooser(intent, "Play video"));
         } catch (ActivityNotFoundException ex) {
             Intent intent = new Intent(Intent.ACTION_VIEW,
                     Uri.parse("http://www.youtube.com/watch?v=" + video.getKey()));
-            startActivity(intent);
+            startActivity(Intent.createChooser(intent, "Play video"));
         }
     }
 
